c6a0f83aed0fe0e40d6294d78f44d9ac740c6336
Shantanu <shantanu24sharma@gmail.com>
2024-04-19 11:25:57 -0400

    Create full project with all functionalities running

--- script/script.js
+++ script/script.js
@@ -1,16 +1,64 @@
-let userInput = 16;
+let userInput = 50;
 
 const theButton = document.querySelector("#btn");
-theButton.addEventListener("click",clickFunction);
+theButton.addEventListener("click",getUserInput);
 
-function clickFunction() {
-    let userInput = window.prompt("What size grid do you want (x*x)?","16");
+createGrid(); //Initial grid size is 16*16 before the user decides to change it
+
+function getUserInput() {
+    
+
+    userInput = window.prompt("What size grid do you want (x*x)? [Enter any number between 1-100]","50");
     userInput = Number(userInput);
-    if ((Number.isInteger(userInput)) && (userInput > 0)){
-        
+
+    
+    if ((Number.isInteger(userInput)) && (userInput > 0) && (userInput <= 100)){
+        createGrid();
+    }
+    else if (userInput == ''){
+        return;
     }
     else{
         window.alert("Invalid Input");
         return;
     }
 }
+
+function createGrid(){
+
+    emptyGrid();
+    
+    for (i=0;i<userInput;i++){
+        const newHorizontalDiv = document.createElement("div");
+        const sketchpadDiv = document.querySelector(".sketchpad");
+
+        sketchpadDiv.appendChild(newHorizontalDiv);
+
+        newHorizontalDiv.style.border = "1px dotted gray";
+        newHorizontalDiv.style.flex = "auto";
+        newHorizontalDiv.style.display = "flex";
+
+        for (j=0;j<userInput;j++){
+            const newVerticalDiv = document.createElement("div");
+            
+            newHorizontalDiv.appendChild(newVerticalDiv);
+
+            newVerticalDiv.style.border = "1px dotted gray";
+            newVerticalDiv.style.flex = "auto";   
+            
+            newVerticalDiv.addEventListener("mouseover",() => {
+                newVerticalDiv.classList.add("hovered");
+            });
+        }
+    }
+}
+
+function emptyGrid(){
+
+    const sketchpadDiv = document.querySelector(".sketchpad");
+
+    while (sketchpadDiv.firstChild){
+        sketchpadDiv.removeChild(sketchpadDiv.lastChild);
+    }
+}
+
